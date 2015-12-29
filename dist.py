4c59ca3274eb4c39e41d7c1517f764f1cb971adc
Denys Smirnov <Denys@MacBook-Air-Denys.local>
2015-12-29 19:07:48 +0200

    Add dist

---
+++ dist.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def dist(l, r):
+	d = 0
+	maxd = 0
+	CHUNK = len(l)
+	def nmin(a, b):
+		if (a > b):
+			return b
+		return a
+	def euclid(a, b):
+		return abs((a - b + 0.0) / CHUNK) + abs(l[a] - r[b])
+	for i in range(CHUNK):
+		mindist = euclid(i, i)
+		for j in range(int(nmin(i, abs(l[i] - r[i])))):
+			if (mindist > euclid(i, i - j)):
+				mindist = euclid(i, i - j)
+		for j in range(int(nmin(CHUNK - i - 1, abs(l[i] - r[i])))):
+			if (mindist > euclid(i, i + j)):
+				mindist = euclid(i, i + j)
+		d += mindist
+		maxd += abs(l[i])
+	return d / maxd
